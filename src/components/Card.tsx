43a7993d10a9b119b18bb30ce2232d9fe9c540f2
Yanai1005 <tatsuya871_1005@icloud.com>
2023-08-27 00:36:13 +0900

    add :show Json

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,34 @@
-
+import { useFetchReview } from '../hooks/useFetchReview';
 
 function Card() {
-    return (
-        <div className="card bg-base-200 flex justify-center">
-            <div className="card-body">
-                <h1 className="card-title">Card title</h1>
-                <div className="rating rating-lg">
-                    <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" />
-                    <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" />
-                    <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" />
-                    <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" />
-                    <input type="radio" name="rating-8" className="mask mask-star-2 bg-orange-400" />
-                </div>
-                <h2 className="">08/26</h2>
-                <h2 className="">スペック</h2>
-                <h3 className="">Review</h3>
-                <p>This MacBook is great!</p>
-            </div>
+  const reviewData = useFetchReview();
+
+  console.log('reviewData:', reviewData);
+
+  return (
+    <div className="card bg-base-200 flex justify-center">
+      <div className="card-body">
+        <h1 className="card-title">Card title</h1>
+        <div className="rating rating-lg">
+          {/* Your rating inputs here */}
         </div>
-    )
+        {reviewData && Object.keys(reviewData).length > 0 && (
+          <>
+            {Object.keys(reviewData).map((key) => (
+              <div key={key}>
+                <h2>{reviewData[key].purchase_date}</h2>
+                <h2>{reviewData[key].review}</h2>
+                <h2>{reviewData[key].model}</h2>
+                <h3>{reviewData[key].title}</h3>
+                <p>{reviewData[key].create_at}</p>
+                <p>{reviewData[key].discription}</p>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Card;
---
+++ src/hooks/useFetchReview.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface ReviewData {
+  [key: string]: {
+    purchase_date: string;
+    review: string;
+    title: string;
+    discription: string;
+    model: string;
+    create_at: string;
+  };
+}
+
+export const useFetchReview = (): ReviewData | null => {
+  const [reviewData, setReviewData] = useState<ReviewData | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://nitari-api.onrender.com/fetchReview');
+        setReviewData(response.data); // 全データを取得
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return reviewData;
+};
