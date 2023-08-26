fcb238a5b527b6234f55f17be6f9c03d3d8e9046
YuSa0-6 <105704944+YuSa0-6@users.noreply.github.com>
2023-08-26 10:12:59 +0000

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div className=''>
+      <div>
         <Routes>
           <Route path="/"element={<Home/>}/>
           <Route path="/inputreview"element={<InputReview/>}/>
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom"
 
 function TopBar(){
     return(
-        <div className="">
+        <div className="w-full  bg-base-300 flex justify-center">
             <Link to="/">
-                <button className="btn btn-ghost">Home</button>
+                <button className="btn btn-ghost flex-1">Home</button>
             </Link>
             <Link to="/inputreview">
-                <button className="btn btn-ghost">InputReview</button>
+                <button className="btn btn-ghost flex-1">InputReview</button>
             </Link>
             <Link to="/review">
-                <button className="btn btn-ghost">Review</button>
+                <button className="btn btn-ghost flex-1">Review</button>
             </Link>
             <Link to="/*">
             </Link>
