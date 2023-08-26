202d0deedb5d2dea3b2cca332ecdce7785a58754
Yanai1005 <tatsuya871_1005@icloud.com>
2023-08-27 01:00:50 +0900

    try

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
-import { createRoot } from 'react-dom/client'; 
-import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom'
+import App from './App.tsx'
+import './index.css'
+import {BrowserRouter} from 'react-router-dom'
 
-import './index.css';
-import './App.css';
+import './App.css'
 
-const root = document.getElementById('root');
-
-if (root) {
-  const appRoot = createRoot(root);
-  appRoot.render(
+ReactDOM.render(
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  );
-} else {
-  console.error("Root element not found");
-}
+    </BrowserRouter>,
+    document.getElementById('root')
+)
