c8138773a232b852ba17588aa4f07fefdc318e25
YuSa0-6 <105704944+YuSa0-6@users.noreply.github.com>
2023-08-26 17:54:05 +0000

    post finished

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,48 +1,59 @@
 import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { postReview } from "./api/postReview.api";
 
 function Form() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const formObject = Object.fromEntries(formData.entries());
-    console.log(formObject);
-  };
-
+  const [cookies, setCookie, removeCookie] = useCookies([
+    "purchase_date",
+    "model",
+  ]);
   const [formState, setFormState] = useState<object>({
     title: "",
-    purchase_date: "",
+    purchase_date: cookies.purchase_date | "情報がありません",
     review: "",
     discription: "",
-    model: "",
+    model: cookies.model | "情報がありません",
     create_at: "",
   });
-
   const [val, setVal] = useState<string>("5");
+
+  const handleSubmit = async () => {
+    console.log("test");
+
+    try {
+      const res = await postReview(formState);
+      if (res) {
+        console.log(res);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   useEffect(() => {
     console.log(formState);
   }, [formState]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <form className="mb-4" onSubmit={handleSubmit}>
-        <label>
-          レビュータイトル
-          <input
-            type="text"
-            name="reviewTitle"
-            placeholder=""
-            className="input input-bordered w-full max-w-xs bg-gray-100"
-            onChange={(e) => {
-              setFormState({ ...formState, title: e.target.value });
-              setVal(e.target.value);
-            }}
-          />
-        </label>
-      </form>
+    <div className="flex flex-col items-center justify-center  p-3 bg-base-100">
+      <label className="">
+        <span className="label-text-text">Review title</span>
+      </label>
+      <input
+        type="text"
+        name="reviewTitle"
+        placeholder="Type here ..."
+        className="input input-bordered w-full max-w-xs bg-base-100"
+        onChange={(e) => {
+          setFormState({ ...formState, title: e.target.value });
+          setVal(e.target.value);
+        }}
+      />
       <div className="mb-4">
-        購入日
-        <input type="date" />
+        <label className="label">
+          <span className="label-text">購入日</span>
+        </label>
+        {formState["purchase_date"]}
       </div>
       <div className="mb-4">
         評価数
@@ -54,68 +65,38 @@ function Form() {
                 key={i}
                 onChange={(e) => {
                   setFormState({ ...formState, review: i + 1 });
-                  setVal(e.target.value)
+                  setVal(e.target.value);
                 }}
                 type="radio"
                 name={`rating-${i + 1}`}
-                className="mask mask-star"
+                className="mask mask-star bg-orange-400"
                 checked={i + 1 === Number(val)}
               />
             );
           })}
         </div>
       </div>
-      <div className="mb-4">
-        スペック
-        <br />
+      <div className="flex flex-col">
+        コメント
+        <textarea
+          onChange={(e) =>
+            setFormState({ ...formState, discription: e.target.value })
+          }
+          className="textarea rounded"
+        />
+      </div>
+      <div>
         モデル
-        <select className="w-full max-w-xs select select-bordered bg-gray-100">
-          <option disabled selected>
-            モデル
-          </option>
-          <option>13インチAir（M1）</option>
-          <option>13インチAir（M2）</option>
-          <option>15インチAir（M2）</option>
-          <option>13インチPro（M2）</option>
-          <option>14インチPro（M2Pro、M2Max）</option>
-          <option>16インチPro（M2Pro、M2Max）</option>
-        </select>
-        <br />
-        メモリ
-        <select className="w-full max-w-xs select select-bordered bg-gray-100">
-          <option disabled selected>
-            メモリ
-          </option>
-          <option>8GB</option>
-          <option>16GB</option>
-          <option>24GB</option>
-        </select>
-        <br />
-        SSD
-        <select className="w-full max-w-xs select select-bordered bg-gray-100">
-          <option disabled selected>
-            SSD
-          </option>
-          <option>256GB</option>
-          <option>512GB</option>
-          <option>1TB</option>
-        </select>
-        <br />
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            レビュー
-            <br />
-            <input
-              type="text"
-              name="reviewText"
-              placeholder=""
-              className="input input-bordered w-full max-w-xs bg-gray-100"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            送信
-          </button>
-        </form>
+        <div>{formState["model"]}</div>
+      </div>
+
+      <div className="mb-4">
+        <button
+          onClick={handleSubmit}
+          className="btn btn-base-999 rounded-full"
+        >
+          送信
+        </button>
       </div>
     </div>
   );
