cd852c568a0a03f6786f65bda937ad2a1c0e7239
YuSa0-6 <105704944+YuSa0-6@users.noreply.github.com>
2023-08-26 14:41:59 +0000

    update styleing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,12 @@
+import Recommend from "../components/Recommend";
 import TopBar from "../components/TopBar";
 
 function Home () {
     return (
         <div>
             <TopBar />
-            <h1>HomePage</h1>
-            <article>
-                <h1>hello</h1>
-            </article>
+            <h1 className="w-full card">MacBook</h1>
+            <Recommend />
         </div>
     )
 }
