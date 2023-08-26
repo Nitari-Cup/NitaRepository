1ee47682deaf32393e47288d56ede0046493f282
Yanai1005 <tatsuya871_1005@icloud.com>
2023-08-26 17:13:08 +0900

    add:Topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
+import Home from './pages/Home'
+import Review from './pages/Review'
+import InputReview from './pages/InputReview'
+import{Routes, Route} from 'react-router-dom'
+import NoMatch from './pages/Nomatch'
 
 function App() {
 
   return (
     <>
       <div>
-        
+        <Routes>
+          <Route path="/"element={<Home/>}/>
+          <Route path="/inputreview"element={<InputReview/>}/>
+          <Route path="/review"element={<Review/>}/>
+          <Route path="/*"element={<NoMatch/>}/>
+        </Routes>
       </div>
     </>
   )
