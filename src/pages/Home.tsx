673c99c67decf7eb662a530b5a0e4f33f19ec661
Yanai1005 <tatsuya871_1005@icloud.com>
2023-08-27 15:35:36 +0900

    try

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,14 @@ import { fetchQuestion } from "../components/api/fetchQuestion.api";
 import { useState, useEffect } from "react";
 import Recommend from "../components/Recommend";
 
+interface Question {
+  id: string;
+  text: string;
+  true: string;
+  false: string;
+  next_question: string;
+}
+
 interface FormState {
   recommended: string;
   next_question: string;
@@ -11,7 +19,7 @@ interface FormState {
 }
 
 function Home() {
-  const [questions, setQuestions] = useState<object>({});
+  const [questions, setQuestions] = useState<{ [key: string]: Question }>({});
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [formState, setFormState] = useState<FormState>({
     recommended: "0",
@@ -45,18 +53,22 @@ function Home() {
           if (
             !isLoading &&
             !formState.is_end &&
-            formState.next_question != "0"
+            formState.next_question !== "0"
           ) {
-            return (
-              <QuestionCard
-                question={questions[Number(formState.next_question)]}
-                index={Number(formState.next_question)}
-                setFormState={setFormState}
-              />
-            );
+            const nextQuestionIndex = formState.next_question;
+            const nextQuestion = questions[nextQuestionIndex];
+            if (nextQuestion) {
+              return (
+                <QuestionCard
+                  question={nextQuestion}
+                  index={Number(nextQuestionIndex)}
+                  setFormState={setFormState}
+                />
+              );
+            }
           } else if (
             (!isLoading && formState.is_end) ||
-            formState.next_question == "0"
+            formState.next_question === "0"
           ) {
             return <Recommend text={formState.recommended} />;
           }
